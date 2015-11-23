549b0d03f6c6f566720224868228df4644cb63b0
Anna Kostenko <blackqueennn@gmail.com>
2015-11-24 00:13:56 +0200

    First view index

--- restaurant_reviews/restaurant_reviews/settings.py
+++ restaurant_reviews/restaurant_reviews/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'restaurant_reviews.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [(os.path.join(BASE_DIR, 'static'), 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- restaurant_reviews/restaurant_reviews/urls.py
+++ restaurant_reviews/restaurant_reviews/urls.py
@@ -17,9 +17,13 @@ from django.conf.urls import include, url, patterns
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', views.index, name='index'),
+
     ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)\
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
