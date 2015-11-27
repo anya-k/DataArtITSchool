5bf071274a141d819a9d43b692d87ab0916dd185
Anna Kostenko <blackqueennn@gmail.com>
2015-11-27 22:21:29 +0200

    Add template and view for restaurant list and detail.

--- restaurant_reviews/restaurant/urls.py
+++ restaurant_reviews/restaurant/urls.py
@@ -4,7 +4,7 @@ from restaurant import views
 
 urlpatterns = patterns('',
     url(r'^$', views.RestaurantListView.as_view(), name='index'),
-    #url(r'^(?P<pk>\d+)/$', views.StudentDetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>\d+)/$', views.RestaurantDetailView.as_view(), name='detail'),
     #url(r'^add_student/$', views.StudentCreateView.as_view(), name='add_student'),
     #url(r'^edit_student/(?P<pk>\d+)/$', views.StudentUpdateView.as_view(), name='edit_student'),
     #url(r'^delete_student/(?P<pk>\d+)/$', views.StudentDeleteView.as_view(), name='delete_student'),
--- restaurant_reviews/restaurant/views.py
+++ restaurant_reviews/restaurant/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from models import Restaurant
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 import logging
 logger = logging.getLogger(__name__)
 
@@ -9,4 +10,6 @@ class RestaurantListView(ListView):
     model = Restaurant
 
 
+class RestaurantDetailView(DetailView):
+    model = Restaurant
 
